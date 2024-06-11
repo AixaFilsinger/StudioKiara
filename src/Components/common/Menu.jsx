e2552f4328f2e3f592be886a75a06f24eaeb5dc5
Aixa Filsinger <macafil99@gmail.com>
2024-06-10 21:49:31 -0300

    se corrigio el error de que el navbar no se cerraba en la version movil cada vez que se apretaba un elemento de este

--- src/Components/common/Menu.jsx
+++ src/Components/common/Menu.jsx
@@ -3,40 +3,44 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import Logo from '../../assets/logofull.png'
+import { useState } from "react";
 
 const Menu = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const closeNav = () => setExpanded(false);
   return (
-    <Navbar className="nav" variant="white" expand="lg">
+    <Navbar className="nav" variant="white" expand="lg" expanded={expanded}>
       <Container>
         <Navbar.Brand as={Link} to={"/"}>
          <div>
           <img src={Logo} alt="kiara studio logo" className="logo" />
          </div>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")}/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <NavLink end className={"nav-item nav-link"} to={"/"}>
+            <NavLink end className={"nav-item nav-link"} to={"/"} onClick={closeNav}>
               Inicio
             </NavLink>
-            <NavLink end className={"nav-item nav-link"} to={"/servicios"}>
+            <NavLink end className={"nav-item nav-link"} to={"/servicios"} onClick={closeNav}>
               Servicios
             </NavLink>
-            <NavLink end className={"nav-item nav-link"} to={"/cursos"}>
+            <NavLink end className={"nav-item nav-link"} to={"/cursos"} onClick={closeNav}>
               Cursos
             </NavLink>
-            <NavLink end className={"nav-item nav-link"} to={"/contacto"}>
+            <NavLink end className={"nav-item nav-link"} to={"/contacto"} onClick={closeNav}>
               Contacto
             </NavLink>
-            <NavLink end className={"nav-item nav-link"} to={"/nosotros"}>
+            <NavLink end className={"nav-item nav-link"} to={"/nosotros"} onClick={closeNav}>
               Nosotros
             </NavLink>
 
-            <NavLink end className={"nav-item nav-link"} to={"/Reservas"}>
+            <NavLink end className={"nav-item nav-link"} to={"/Reservas"} onClick={closeNav}>
               Reservas
             </NavLink>
 
-            <NavLink end className={"nav-item nav-link"} to={"/login"}>
+            <NavLink end className={"nav-item nav-link"} to={"/login"} onClick={closeNav}>
               Login
             </NavLink>
           </Nav>

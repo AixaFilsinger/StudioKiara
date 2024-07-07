6de2ed376602005ab7b924454bdf201b5d6d3d6b
Aixa Filsinger <macafil99@gmail.com>
2024-07-07 12:07:48 -0300

    se estructuro el componente cursos

--- src/Components/views/Cursos.jsx
+++ src/Components/views/Cursos.jsx
@@ -1,9 +1,45 @@
-import React from "react";
-
 
 const Cursos = () => {
-
-
+    
+  return (
+    <section>
+      <h1>Nuestros cursos</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus qui
+        sunt culpa animi porro enim sint adipisci accusantium, odio minima,
+        obcaecati quibusdam tempora error? Eos, similique, perferendis iusto
+        repudiandae eius sit odio omnis, praesentium voluptas blanditiis
+        facilis. Doloremque quos fugiat doloribus, aliquam sapiente quam atque
+        repudiandae sunt asperiores corrupti? Nostrum totam laboriosam nulla
+        officia quia, harum error debitis. Ratione, cumque est, ab rem sed a
+        deleniti perspiciatis saepe repellendus cupiditate ea. Nihil error,
+        delectus quis vel unde impedit ullam quo iure nemo recusandae quas
+        itaque, voluptatibus perspiciatis voluptates distinctio aperiam culpa
+        aut aliquam mollitia a et velit accusantium! Accusamus, dolorem.
+      </p>
+      <article>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col">
+            <div className="card flex justify-content-center">
+            <Card title="Advanced Card" subTitle="Card subtitle" footer={footer} header={header} className="md:w-25rem">
+                <p className="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                    numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                </p>
+            </Card>
+        </div>
+            </div>
+            <div className="col">
+            
+            </div>
+            <div className="col">Column</div>
+          </div>
+        </div>
+      </article>
+      
+    </section>
+  );
 };
 
 export default Cursos;

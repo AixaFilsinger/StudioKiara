486660238baa9b9a4d0ab0879bb76dfd77d8a6d1
Aixa Filsinger <macafil99@gmail.com>
2024-07-13 17:22:06 -0300

    Probando estilos

--- src/Components/views/Cursos.jsx
+++ src/Components/views/Cursos.jsx
@@ -30,8 +30,36 @@ const Cursos = () => {
                 </Card.Body>
               </Card>
             </div>
+            <div className="col-sm-12 col-md-4">
+              <Card style={{ width: "16rem", border: "1px solid #FFD700" }} className="borde-card">
+                <Card.Img variant="top" src={uñas} />
+                <Card.Body className="bg-card-cursos ">
+                  <Card.Title className="fw-semibold">Manicura</Card.Title>
+                  <Card.Text>
+                    
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button size="lg" className="btn-cursos">Inscribirse</Button>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-sm-12 col-md-4">
+              <Card style={{ width: "16rem", border: "1px solid #FFD700" }} className="borde-card">
+                <Card.Img variant="top" src={uñas} />
+                <Card.Body className="bg-card-cursos ">
+                  <Card.Title className="fw-semibold">Manicura</Card.Title>
+                  <Card.Text>
+                    
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button size="lg" className="btn-cursos">Inscribirse</Button>
+                </Card.Body>
+              </Card>
+            </div>
             <div className="col-sm-12 col-md-4"></div>
-            <div className="col-sm-12 col-md-4">Column</div>
+           
           </div>
         </div>
       </article>
